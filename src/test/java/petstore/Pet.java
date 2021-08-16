331cc88b0a1a5e7c4646b25721235d6b981fa00d
Rivanildo <rivanildo_duarte@yahoo.com.br>
2021-08-16 12:13:19 -0400

    Criei um teste para incluir um Pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,62 @@
+//1. Pacotes
+package petstore;
+
+// 2. Bibliotecas
+
+// 3. Classes
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Pet {
+
+    //3.1 Atributos
+
+    String uri = "https://petstore.swagger.io/v2/pet"; // Endereço da entidade Pet
+
+
+    //3.2 Métodos e Funções
+    public String lerJson(String caminhoJson) throws IOException {
+
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+
+    }
+    //Incluir - create - Post
+    @Test// identifica o metodo ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
+
+        String jsonBody = lerJson("data_base/pet1.json" );
+
+        //sintaxe Gherkin (giria pepino e conserva = problema de comunicação
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given()//Dado
+
+                .contentType("application/json")// comum em API REST - antigas era "text/xml
+                .log().all()
+                .body(jsonBody)
+
+        .when()//Quando
+                .post(uri)
+
+        .then()// Então
+                .log().all()
+                .statusCode(200)
+        ;
+
+
+    }
+
+}
+
+
+
+
+
